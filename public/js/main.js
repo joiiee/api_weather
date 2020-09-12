8cdff433892cf0807a9e8d8b83b7656c7856a874
jcabria <julie.cabria@gmail.com>
2020-09-13 03:19:16 +0800

    push weather api

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+
+const apiData = {
+    url: "http://api.openweathermap.org/data/2.5/",
+    type: "weather",
+    city: "Quezon City",
+    units: "metric",
+    appId: "7cdb59565e2298911f177546e7c34a70"
+}
+
+const { url, type, city, units, appId } = apiData;
+const apiUrl = `${url}${type}?q=${encodeURIComponent(city)}&units=${units}&appId=${appId}`;
+const weatherWrapper = document.querySelector('.weather-wrapper');
+
+async function showWeather() {
+    let res = await fetch(apiUrl);
+    let weather = await res.json();
+    
+    const addHtml = data => {
+        const desc = data.weather[0].description;
+        const urlImg = 'http://openweathermap.org/img/wn/';
+        const icon = `${urlImg}${data.weather[0].icon}@2x.png`;
+    
+        const html = `
+                <p class="cityCountry">${data.name}, ${data.sys.country}</p>
+                <img alt="Weather Icon" class="icon" src="${icon}">
+                <p class="weather">${desc[0].toUpperCase() + desc.substring(1)}</p>
+                <p class="temp">${data.main.temp}&#8451;</p>
+            `;    
+        
+        weatherWrapper.innerHTML = html;
+    }
+
+    let details = addHtml(weather);
+}
+
+showWeather();
